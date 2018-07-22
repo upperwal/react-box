1aac530e3e9d026dbce02f0fd4c05cfe0689f64d
Abhishek Upperwal <a.upperwal@gmail.com>
2018-07-23 02:14:41 +0530

    Added support for web3 v1.0.x

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.4.18;
 contract SimpleStorage {
   uint storedData;
 
+  event StorageVal(uint data);
+
   function set(uint x) public {
     storedData = x;
+
+    emit StorageVal(x);
   }
 
   function get() public view returns (uint) {
--- src/App.js
+++ src/App.js
@@ -8,87 +8,82 @@ import './css/pure-min.css'
 import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      storageValue: 0,
-      web3: null
+        this.state = {
+            storageValue: 0,
+            web3: null
+        }
     }
-  }
 
-  componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+    componentWillMount() {
+        // Get network provider and web3 instance.
+        // See utils/getWeb3 for more info.
 
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
+        getWeb3.then(results => {
+            this.setState({
+                web3: results.web3
+            }, () => {
+                // Instantiate contract once web3 provided.
+                this.instantiateContract()
+            })
+            
+        })
+    }
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
-  }
+    instantiateContract() {
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
+        var simpleStorageInstance = new this.state.web3.eth.Contract(SimpleStorageContract.abi, SimpleStorageContract.networks['5777'].address)
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+        var accountsList;
+        var self = this;
+        // Get accounts.
+        this.state.web3.eth.getAccounts((error, accounts) => {
+            // Stores a given value, 5 by default.
+            accountsList = accounts
+            console.log(accountsList)
+            
+            simpleStorageInstance.methods.set(5).send({from: accountsList[0]}).then(transHash => {
+                console.log(transHash)
+                // Get the value from the contract to prove it worked.
+                return simpleStorageInstance.methods.get().call({from: accountsList[0]})
+            }).then(result => {
+                // Update state with the result.
+                console.log('final result: ' + result)
+                return self.setState({ storageValue: result })
+            })
+        })
+        
+        // Subscribe to StorageVal event to get updates
+        simpleStorageInstance.events.StorageVal((error, result) => {
+            console.log(result)
+        })
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    }
 
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar pure-menu pure-menu-horizontal">
+                    <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+                </nav>
 
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+                <main className="container">
+                    <div className="pure-g">
+                        <div className="pure-u-1-1">
+                            <h1>Good to Go!</h1>
+                            <p>Your Truffle Box is installed and ready.</p>
+                            <h2>Smart Contract Example</h2>
+                            <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+                            <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+                            <p>The stored value is: {this.state.storageValue}</p>
+                        </div>
+                    </div>
+                </main>
             </div>
-          </div>
-        </main>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App
